ec5e66a796b2fc48afea5cb3b467bef3d398d464
Johnny Chiu <90211190+dinsky21@users.noreply.github.com>
2023-02-17 14:47:00 +0800

    Remove the card when clicking delete-drink icon

--- app.js
+++ app.js
@@ -1,48 +1,49 @@
 // Constructor function for Drinks
-function Drink (name, sugar, ice) {
-  this.name = name
-  this.sugar = sugar
-  this.ice = ice
+function Drink(name, sugar, ice) {
+  this.name = name;
+  this.sugar = sugar;
+  this.ice = ice;
 }
 Drink.prototype.price = function () {
   switch (this.name) {
-    case 'Black Tea':
-    case 'Oolong Tea':
-    case 'Baozong Tea':
-    case 'Green Tea':
-      return 30
-    case 'Bubble Milk Tea':
-    case 'Lemon Green Tea':
-      return 50
-    case 'Black Tea Latte':
-    case 'Matcha Latte':
-      return 55
+    case "Black Tea":
+    case "Oolong Tea":
+    case "Baozong Tea":
+    case "Green Tea":
+      return 30;
+    case "Bubble Milk Tea":
+    case "Lemon Green Tea":
+      return 50;
+    case "Black Tea Latte":
+    case "Matcha Latte":
+      return 55;
     default:
-      alert('No this drink')
+      alert("No this drink");
   }
-}
+};
 // AlphaPos Constructor Function
-function AlphaPos () {}
-const alphaPos = new AlphaPos()
+function AlphaPos() {}
+const alphaPos = new AlphaPos();
 
-const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]')
-addDrinkButton.addEventListener('click', function() {
+// 新增飲料
+const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]');
+addDrinkButton.addEventListener("click", () => {
   // 1. 取得店員選擇的飲料品項、甜度、冰塊選項內容
-  const drinkName = alphaPos.getCheckedValue('drink')
-  const ice = alphaPos.getCheckedValue('ice')
-  const sugar = alphaPos.getCheckedValue('sugar')
-  console.log(`${drinkName}, ${ice}, ${sugar}`)
+  const drinkName = alphaPos.getCheckedValue("drink");
+  const ice = alphaPos.getCheckedValue("ice");
+  const sugar = alphaPos.getCheckedValue("sugar");
+  // console.log(`${drinkName}, ${ice}, ${sugar}`)
 
   // 2. 如果沒有選擇飲料品項，跳出提示
-	if (!drinkName) {
-  alert('Please choose at least one item.')
-  return
-}
+  if (!drinkName) {
+    alert("Please choose at least one item.");
+    return;
+  }
   // 3. 建立飲料實例，並取得飲料價格
-	const drink = new Drink(drinkName, sugar, ice)
+  const drink = new Drink(drinkName, sugar, ice);
   // 4. 將飲料實例產生成左側訂單區的畫面
-	alphaPos.addDrink(drink)
-})
+  alphaPos.addDrink(drink);
+});
 
 // let allDrinksOptions = document.querySelectorAll('input[name="drink"]')
 // allDrinksOptions.forEach(function(option){
@@ -61,20 +62,19 @@ addDrinkButton.addEventListener('click', function() {
 
 // 上面三個可以濃縮成下方，利用Constructor來表達，再放到上方EventListener去觸發
 
-
 AlphaPos.prototype.getCheckedValue = function (inputName) {
-	let selectedOption = ''
-	document.querySelectorAll(`[name=${inputName}]`).forEach(function(item){
-		if (item.checked) {
-			selectedOption = item.value		
-		}
-	})
-	return selectedOption
-}
+  let selectedOption = "";
+  document.querySelectorAll(`[name=${inputName}]`).forEach((item) => {
+    if (item.checked) {
+      selectedOption = item.value;
+    }
+  });
+  return selectedOption;
+};
 
-const orderLists = document.querySelector('[data-order-lists]')
-	AlphaPos.prototype.addDrink = function (drink) {
-  let orderListsCard = `
+const orderLists = document.querySelector("[data-order-lists]"); // 拿到左側爛位，準備填入右側輸出的訂單卡片
+AlphaPos.prototype.addDrink = function (drink) {
+  const orderListsCard = `
     <div class="card mb-3">
     <div class="card-body pt-3 pr-3">
       <div class="text-right">
@@ -88,7 +88,19 @@ const orderLists = document.querySelector('[data-order-lists]')
       <div class="card-text text-muted">$ <span data-drink-price>${drink.price()}</span></div>
     </div>
   </div>
-  `
-  orderLists.insertAdjacentHTML('afterbegin', orderListsCard)
-}
+  `;
+  orderLists.insertAdjacentHTML("afterbegin", orderListsCard);
+};
 
+// 刪除左側卡片
+orderLists.addEventListener("click", (event) => {
+  let isDeleteButton = event.target.matches('[data-alpha-pos="delete-drink"]');
+  if (!isDeleteButton) {
+    return;
+  }
+  alphaPos.deleteDrink(event.target.parentElement.parentElement.parentElement);
+});
+
+AlphaPos.prototype.deleteDrink = function (target) {
+  target.remove();
+};
